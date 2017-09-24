125ef626cd48e66b0c7f3c175ac92c5c70b80589
bira91 <chilled.bira91@yandex.com>
2017-09-24 20:00:09 +0530

    initial commit

---
+++ assets/js/list.js
@@ -0,0 +1,18 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+//click on X to delete todo
+$("ul").on("click","span",function(event){
+	$(this).parent().remove();
+	event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13)
+	{
+		// grabbing the new todo text from input
+		var todoText = ($(this).val());
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li>"+"<span>X </span>"+todoText+"</li>");
+	}
+});
